0b0d7e2f25c7c6f6d470b918cfba970057c21138
AlexBeltran31 <alexbeltranrt@gmail.com>
2025-09-23 13:54:15 +0200

    Exercise 2 done

--- index.php
+++ index.php
@@ -25,3 +25,32 @@ $employee1->printTaxInfo();
 $employee2 = new Employee();
 $employee2->setEmployeeInfo("Amir", 5000);
 $employee2->printTaxInfo();
+
+// ex 2
+class Shape {
+    protected int $width;
+    protected int $height;
+
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+}
+
+class Triangle extends Shape {
+    public function getArea() {
+        return ($this->width * $this->height) / 2;
+    }
+}
+
+class Rectangle extends Shape {
+    public function getArea() {
+        return $this->width * $this->height;
+    }
+}
+
+$triangle = new Triangle(10, 5);
+echo "Triangle area: " . $triangle->getArea() . "\n";
+
+$rectangle = new Rectangle(30, 15);
+echo "Rectangle area: " . $rectangle->getArea() . "\n";
