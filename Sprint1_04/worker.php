06d15dd8a54b8747bb4b95f6e6a0ce8e6bea1d9e
AlexBeltran31 <alexbeltranrt@gmail.com>
2025-09-23 11:42:44 +0200

    Exercise 1 done

--- Sprint1_04/worker.php
+++ Sprint1_04/worker.php
@@ -1,1 +1,28 @@
 <?php
+
+class worker {
+    private string $name;
+    private int $salary;
+
+    public function setWorkersInfo($name, $salary) {
+        $this->name = $name;
+        $this->salary = $salary;
+    }
+
+    public function printTaxInfo() {
+        echo "Worker: " . $this->name . "\n";
+        if ($this->salary > 6000) {
+            echo "This worker must pay taxes.\n";
+        } else {
+            echo "This worker doen't have to pay taxes.\n";
+        }
+    }
+}
+
+$worker1 = new worker();
+$worker1->setWorkersInfo("Camila", 7000);
+$worker1->printTaxInfo();
+
+$worker2 = new worker();
+$worker2->setWorkersInfo("Amir", 3000);
+$worker2->printTaxInfo();
